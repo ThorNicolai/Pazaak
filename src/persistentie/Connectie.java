bbc4b7e5165fe5736fe38af6ffb0e4272ff9cb2d
Thor Nicolaï <thor.nicolai.w3471@student.hogent.be>
2017-02-26 16:43:02 +0100

    Connectie in de persistentielaag aanmaken. Databank zou gelinkt moeten zijn

---
+++ src/persistentie/Connectie.java
@@ -0,0 +1,9 @@
+package persistentie;
+
+public class Connectie {
+
+    /*
+     * INFO: Hieronder dien je uiteraard het schema, de username en het paswoord nog aan te passen.
+     */
+    public static final String JDBC_URL = "jdbc:mysql://ID222177_g42.db.webhosting.be?user=ID222177_g42&password=RynIcvObAin3";
+}
