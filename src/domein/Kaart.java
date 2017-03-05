e5fc7f982e0f836f5c1c4edee47fa0730624b66b
Tristan Nieto <tristan.nietomarquina.y7264@student.hogent.be>
2017-03-05 15:42:06 +0100

    Merge origin/master
    Conflicts:
    	src/domein/DomeinController.java
    	src/domein/Speler.java

--- src/domein/Kaart.java
+++ src/domein/Kaart.java
@@ -1,10 +1,32 @@
 package domein;
 
 public class Kaart {
-    
+
     private String kaarttype;
     private int waarde;
-    
-    
-    
+
+    public Kaart(String kaarttype, int waarde) {
+        controleerKaartType(kaarttype);
+        this.kaarttype = kaarttype;
+
+        controleerWaarde(waarde);
+        this.waarde = waarde;
+    }
+
+    private void controleerKaartType(String kaartType) {
+
+        if (kaartType != "+" && kaartType != "-" && kaartType != "+/-") {
+            throw new IllegalArgumentException("Het kaarttype bestaat niet!");
+        }
+
+    }
+
+    private void controleerWaarde(int waarde) {
+
+        if (waarde < 1 || waarde > 6) {
+            throw new IllegalArgumentException("De waarde van de kaart is te laag of te hoog");
+        }
+
+    }
+
 }
--- src/domein/Speler.java
+++ src/domein/Speler.java
@@ -1,31 +1,40 @@
 package domein;
 
+import java.util.Calendar;
+
 public class Speler {
     
     private String naam;
     private int geboortejaar;
     private int krediet;
+    int year = Calendar.getInstance().get(Calendar.YEAR);
     
     public Speler(String naam, int geboortejaar, int krediet){
-        this.naam = naam;
-        this.geboortejaar = geboortejaar;
+        
+        
         this.krediet = krediet;
         
         controleerNaam(naam);
+        this.naam = naam;
         controleerLeeftijd(geboortejaar);
+        this.geboortejaar = geboortejaar;
         
         
     }
     
     private void controleerNaam(String naam){
         
-        if(naam.length() < 3){
+        if(naam.length() < 3 || Character.isDigit(naam.charAt(0)) ){
             throw new IllegalArgumentException("De naam moet minimum 3 karakters lang zijn.");
         }
         
     }
     
     private void controleerLeeftijd(int geboortejaar){
+        
+        if(year - geboortejaar < 6 || year - geboortejaar > 99){
+            throw new IllegalArgumentException("Je bent te jong om te spelen. Je moet minstens 6 jaar zijn!");
+        }
            
     }
 
