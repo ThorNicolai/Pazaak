1cdc15c9401c53b3e31f84cd0c5f6afd9a2a3739
Thor Nicolaï <thor.nicolai.w3471@student.hogent.be>
2017-03-13 18:25:24 +0100

    Kleine aanpassingen

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -1,19 +1,26 @@
 package domein;
 
+import java.util.ResourceBundle;
+import resources.Taal;
+
 public class DomeinController {
-    
+
     private SpelerRepository spelerrepo;
-    
-    public DomeinController(){
+    private Taal taal;
+
+    public DomeinController() {
         spelerrepo = new SpelerRepository();
     }
-    
-    public void voegSpelerToe(String naam, int geboortejaar){
-        
+
+    public void voegSpelerToe(String naam, int geboortejaar) {
+
         spelerrepo.voegSpelerToe(naam, geboortejaar); //roept methode voegSpelerToe aan uit SpelerRepository(waarin gecontroleerd wordt of naam al bestaat)
-        
+
     }
-    
+
+    public void kiesTaal(int keuze) {
+        taal = new Taal(keuze);
+    }
 
     
  
