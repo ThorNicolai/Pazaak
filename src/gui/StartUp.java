8fd530b3f7d57744cb32d9ddadf87d3d2e8491ed
Tristan Nieto <tristan.nietomarquina.y7264@student.hogent.be>
2017-03-14 17:32:14 +0100

    fuck you github

--- src/gui/StartUp.java
+++ src/gui/StartUp.java
@@ -1,21 +1,42 @@
 package gui;
 
 import domein.DomeinController;
+import domein.Speler;
 import java.util.Scanner;
 
 public class StartUp {
-    
+
     public static void main(String[] args) {
-        
+
         DomeinController dc = new DomeinController();
-        
         Scanner input = new Scanner(System.in);
-        System.out.println("Geef uw gebruikersnaam in: ");
-        String naam = input.nextLine();
-        System.out.println("Geef uw geboortejaar in: ");
-        int geboortejaar = input.nextInt();
-        
-        dc.voegSpelerToe(naam, geboortejaar); //roept methode voegSpelerToe aan uit DC
-        
+        String naam;
+        int geboortejaar;
+        boolean flag1 = true;
+        boolean flag2 = true;
+
+        do {
+
+            try {
+
+                System.out.print("Geef uw gebruikersnaam in: ");
+                naam = input.nextLine();
+
+                System.out.print("Geef uw geboortejaar in: ");
+                geboortejaar = input.nextInt();
+
+                Speler speler = new Speler(naam, geboortejaar, 0);
+
+                flag1 = false;
+
+            } catch (IllegalArgumentException e) {
+
+                System.out.printf("%s%n%n", e.getMessage());
+                input.nextLine();
+
+            }
+        } while (flag1);
+
     }
+
 }
