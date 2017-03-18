347ea9157bae2f3b4c5105860f96a92540d7462e
Jelle Callewaert <jelle.callewaert.y9088@student.hogent.be>
2017-03-18 16:32:29 +0100

    mysql implementatie

--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -1,46 +1,62 @@
 package persistentie;
 
 import domein.Speler;
+//import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+//import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SpelerMapper {
     
-    List<Speler> spelerlijst = new ArrayList<>();
+    
+    public void voegToe(Speler speler) {
 
-    public SpelerMapper(){
-    
-    
-        Speler speler1 = new Speler("Jan",1999,50);
-        Speler speler2 = new Speler("Piet",1989,10);
-        Speler speler3 = new Speler("Joris",1987,100);
-        Speler speler4 = new Speler("Korneel",1992,15);
-        
-        spelerlijst.add(speler1);
-        spelerlijst.add(speler2);
-        spelerlijst.add(speler3);
-        spelerlijst.add(speler4);
-        
-    
-    }
-    
-    public void voegSpelerToe(Speler speler){
-        spelerlijst.add(speler);
-    }
+            try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL)) {
+                PreparedStatement query = conn.prepareStatement("INSERT INTO ID222177_dobbel.speler (naam, voornaam, emailadres, geboortedatum, wachtwoord, beheerder, krediet)"
+                        + "VALUES (?, ?, ?, ?, ?, ?, ?)");
+                query.setString(1, speler.getNaam());
+//                query.setString(2, speler.getVoornaam());
+//                query.setString(3, speler.getEmailadres());
+                query.setInt(4, speler.getGeboortejaar());
+//                query.setString(5, speler.getWachtwoord());
+//                query.setBoolean(6, speler.isAdminrechten());
+                query.setInt(7, speler.getKrediet());
+                query.executeUpdate();
 
-//    public String[] geefAlleNamen(List<Speler> spelerlijst) {
-//
-//        String spelernaamarray[] = new String[10]; //voorlopig lengte op 10 ingesteld
-//
-//        for (int i = 0; i < spelerlijst.size(); i++) { //overloopt de spelerlijst
-//            spelernaamarray[i] = spelerlijst.get(i).getNaam(); //steekt de naam van speler i in spelernaamarray[i]
-//        }
-//
-//        return spelernaamarray;
-//    }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
 
-    public List<Speler> getSpelerlijst() {
-        return spelerlijst;
+
+public List<Speler> geefSpelers() {
+        List<Speler> spelers = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL)) {
+            PreparedStatement query = conn.prepareStatement("SELECT * FROM ID222177_dobbel.speler");
+            try (ResultSet rs = query.executeQuery()) {
+                while (rs.next()) {
+                    String naam = rs.getString("naam");
+//                    String voornaam = rs.getString("voornaam");
+//                    String emailadres = rs.getString("emailadres");
+                    int geboortejaar = rs.getInt("geboortedatum");
+//                    String wachtwoord = rs.getString("wachtwoord");
+//                    boolean beheerder = rs.getBoolean("beheerder");
+                    int krediet = rs.getInt("krediet");
+
+                    spelers.add(new Speler(naam, /*voornaam, emailadres,*/ geboortejaar, /*wachtwoord, beheerder,*/ krediet));
+                }
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return spelers;
     }
 
 }
