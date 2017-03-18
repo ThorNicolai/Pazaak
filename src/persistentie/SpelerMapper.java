07b7335cbad8719ca23c4781c8b2f30c6ac0e7c3
Jelle Callewaert <jelle.callewaert.y9088@student.hogent.be>
2017-03-18 16:34:33 +0100

    mysql implementatie

--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -1,13 +1,11 @@
 package persistentie;
 
 import domein.Speler;
-//import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-//import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,12 +18,8 @@ public class SpelerMapper {
                 PreparedStatement query = conn.prepareStatement("INSERT INTO ID222177_dobbel.speler (naam, voornaam, emailadres, geboortedatum, wachtwoord, beheerder, krediet)"
                         + "VALUES (?, ?, ?, ?, ?, ?, ?)");
                 query.setString(1, speler.getNaam());
-//                query.setString(2, speler.getVoornaam());
-//                query.setString(3, speler.getEmailadres());
-                query.setInt(4, speler.getGeboortejaar());
-//                query.setString(5, speler.getWachtwoord());
-//                query.setBoolean(6, speler.isAdminrechten());
-                query.setInt(7, speler.getKrediet());
+                query.setInt(2, speler.getGeboortejaar());
+                query.setInt(3, speler.getKrediet());
                 query.executeUpdate();
 
             } catch (SQLException ex) {
@@ -42,14 +36,10 @@ public List<Speler> geefSpelers() {
             try (ResultSet rs = query.executeQuery()) {
                 while (rs.next()) {
                     String naam = rs.getString("naam");
-//                    String voornaam = rs.getString("voornaam");
-//                    String emailadres = rs.getString("emailadres");
                     int geboortejaar = rs.getInt("geboortedatum");
-//                    String wachtwoord = rs.getString("wachtwoord");
-//                    boolean beheerder = rs.getBoolean("beheerder");
                     int krediet = rs.getInt("krediet");
 
-                    spelers.add(new Speler(naam, /*voornaam, emailadres,*/ geboortejaar, /*wachtwoord, beheerder,*/ krediet));
+                    spelers.add(new Speler(naam, geboortejaar, krediet));
                 }
             }
         } catch (SQLException ex) {
