d2180be7b9ca4d46b4734d22df86a5e8838075a8
Tristan Nieto <tristan.nietomarquina.y7264@student.hogent.be>
2017-03-20 18:02:23 +0100

    code van startup verplaatst naar de applicatieklasse

--- src/gui/StartUp.java
+++ src/gui/StartUp.java
@@ -1,41 +1,15 @@
     package gui;
 
-import domein.DomeinController;
-import domein.Speler;
-import java.util.Scanner;
+
+import ui.pazaakApplicatie2;
 
 public class StartUp {
 
     public static void main(String[] args) {
-
-        DomeinController dc = new DomeinController();
-        Scanner input = new Scanner(System.in);
-        String naam;
-        int geboortejaar;
-        boolean flag1 = true;
-        boolean flag2 = true;
-
-        do {
-
-            try {
-
-                System.out.print("Geef uw gebruikersnaam in: ");
-                naam = input.nextLine();
-
-                System.out.print("Geef uw geboortejaar in: ");
-                geboortejaar = input.nextInt();
-
-                Speler speler = new Speler(naam, geboortejaar, 0);
-
-                flag1 = false;
-
-            } catch (IllegalArgumentException e) {
-
-                System.out.printf("%s%n%n", e.getMessage());
-                input.nextLine();
-
-            }
-        } while (flag1);
+        
+        
+        pazaakApplicatie2 pa2 = new pazaakApplicatie2();
+        pa2.start();
 
     }
 
