139c05b32b7f9dac79b3af4dd7127e1844c1cc28
Thor Nicolaï <thor.nicolai.w3471@student.hogent.be>
2017-03-21 14:22:24 +0100

    Spelermapper

--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -1,52 +1,54 @@
 package persistentie;
 
 import domein.Speler;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SpelerMapper {
-    
-    
+
     public void voegToe(Speler speler) {
 
-            try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL)) {
-                PreparedStatement query = conn.prepareStatement("INSERT INTO ID222177_dobbel.speler (naam, voornaam, emailadres, geboortedatum, wachtwoord, beheerder, krediet)"
-                        + "VALUES (?, ?, ?, ?, ?, ?, ?)");
-                query.setString(1, speler.getNaam());
-                query.setInt(2, speler.getGeboortejaar());
-                query.setInt(3, speler.getKrediet());
-                query.executeUpdate();
+        try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL)) {
+            PreparedStatement query = conn.prepareStatement("INSERT INTO ID222177_g42.db.webhosting.be (naam, geboortedatum, krediet)"
+                    + "VALUES (?, ?, ?)");
+            query.setString(1, speler.getNaam());
+            query.setInt(2, speler.getGeboortejaar());
+            query.setInt(3, speler.getKrediet());
+            query.executeUpdate();
 
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
         }
 
+    }
 
-public List<Speler> geefSpelers() {
-        List<Speler> spelers = new ArrayList<>();
+    public Speler geefSpeler(String naam) {
+
+        Speler speler = null;
 
         try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL)) {
-            PreparedStatement query = conn.prepareStatement("SELECT * FROM ID222177_dobbel.speler");
+            PreparedStatement query = conn.prepareStatement("SELECT * FROM ID222177_g42.db.webhosting.be WHERE naam = ?");
+            query.setString(1, naam);
             try (ResultSet rs = query.executeQuery()) {
-                while (rs.next()) {
-                    String naam = rs.getString("naam");
-                    int geboortejaar = rs.getInt("geboortedatum");
+                if (rs.next()) {
+                    int geboortejaar = rs.getInt("geboortejaar");
                     int krediet = rs.getInt("krediet");
 
-                    spelers.add(new Speler(naam, geboortejaar, krediet));
+                    speler = new Speler(naam, geboortejaar, krediet);
+
                 }
             }
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
 
-        return spelers;
+        return speler;
     }
-
 }
